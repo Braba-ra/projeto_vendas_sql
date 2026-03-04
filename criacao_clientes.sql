c5c922eea546636dbe91fe8a9f3acb8b3081f8f0
Bárbara Letícia Soares <barbaracavalcanti665@gmail.com>
2026-03-04 08:44:47 -0300

    Update criacao_clientes.sql

--- criacao_clientes.sql
+++ criacao_clientes.sql
@@ -3,7 +3,8 @@
 create table clientes (
 id_cliente int primary key auto_increment
   nome varchar (50) not null,
-  email varchar (50) not null  
+  email varchar (50) not null,
+  data_cadastro DATE
 );
 
 
