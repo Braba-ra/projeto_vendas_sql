610628e141d0423752b8f444c00e01d141333d96
Bárbara Letícia Soares <barbaracavalcanti665@gmail.com>
2026-03-04 08:43:34 -0300

    Create criacao_clientes.sql
    CRIAÇÃO DA ESTRUTURA BÁSICA DE CLIENTES

---
+++ criacao_clientes.sql
@@ -0,0 +1,10 @@
+# CRIAÇÃO DA ESTRUTURA BÁSICA DE CLIENTES.
+
+create table clientes (
+id_cliente int primary key auto_increment
+  nome varchar (50) not null,
+  email varchar (50) not null  
+);
+
+
+
